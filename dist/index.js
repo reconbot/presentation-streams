393eb3796a43db0bb411c46beecf682cc799a7d1
Francis Gulotta <wizard@roborooter.com>
2017-09-05 14:19:04 -0400

    build out the actual async iterator of an array

---
+++ dist/index.js
@@ -0,0 +1,18 @@
+"use strict";
+
+function asyncArray(arr) {
+  const iArr = arr[Symbol.iterator]();
+  return {
+    async next() {
+      return iArr.next();
+    }
+  };
+}
+
+async function count() {
+  for await (const line of asyncArray([1, 2, 3, 4, 5])) {
+    console.log(number);
+  }
+}
+
+count();
